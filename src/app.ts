fa984eb75d1fd9fe08382eb6ec1ccc44b9f0601d
Matheus Costa <matheuscostadosantos@hotmail.com>
2021-10-19 00:28:06 -0300

    Comentarios app.ts

--- src/app.ts
+++ src/app.ts
@@ -3,14 +3,16 @@ import express, { request } from "express";
 
 const app = express();
 
+// Rota github (localhost:4000/github)
 app.get("/github", (request, response) => {
-  response.redirect(`https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}`)
+  response.redirect(`https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}`) // vai no github e faz a autenticação
 })
 
-// Criando a rota
+// Rota de callback (localhost:4000/signin/callback)
 app.get("/signin/callback", (request, response) => {
   const { code } = request.query;
-  return response.json(code);
+  return response.json(code); // exibe o código de retorno do github
 })
 
+// Inicia o servidor na porta 4000
 app.listen(4000, () => console.log(`🚀 Servidor rodando na porta 4000`))
