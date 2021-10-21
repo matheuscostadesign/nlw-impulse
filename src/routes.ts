7560af1b228cd8542024464316bb9dbac78fb48d
Matheus Costa <matheuscostadosantos@hotmail.com>
2021-10-21 00:06:05 -0300

    Criação GetLast3Messages Service/Controller

--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { AuthenticateUserController } from "./controllers/AuthenticateUserController";
 import { CreateMessageController } from "./controllers/CreateMessageController";
+import { GetLast3MessagesController } from "./controllers/GetLast3MessagesController";
 import { ensureAuthenticated } from "./middleware/ensureAuthenticated";
 
 const router = Router();
@@ -8,7 +9,10 @@ const router = Router();
 // Rota authenticate
 router.post("/authenticate", new AuthenticateUserController().handle)
 
-// Rota
+// Rota messages
 router.post("/messages", ensureAuthenticated, new CreateMessageController().handle)
+
+// Rota messages/last3
+router.get("/messages/last3", new GetLast3MessagesController().handle)
 
 export { router }
